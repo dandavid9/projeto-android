f6c113bc6f7c388ee7dfe46483e7ef9b5c312bd2
Danilo David <220001205@esg.ipsantarem.pt>
2024-01-10 00:20:03 +0000

    Adicionar icone no perfil do utilizador

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,5 +57,8 @@ dependencies {
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation ("org.tensorflow:tensorflow-lite-task-vision:0.3.1")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+
 
 }
--- app/src/main/java/com/example/projeto/TelaUtilizador.kt
+++ app/src/main/java/com/example/projeto/TelaUtilizador.kt
@@ -1,22 +1,19 @@
 package com.example.projeto
 
-import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.bumptech.glide.request.RequestOptions
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthRegistrar
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
@@ -27,22 +24,20 @@ class TelaUtilizador : AppCompatActivity() {
 
     private lateinit var userName: TextView
     private lateinit var userEmail: TextView
-    private lateinit var userImage: ImageView
     private lateinit var btnDeslogar: Button
+    private lateinit var userImage: ImageView
     private var db: FirebaseFirestore = FirebaseFirestore.getInstance()
     lateinit var utilizadorID: String
-    private val pickImage =
+    private val captureImage =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
+            if (result.resultCode == RESULT_OK) {
                 val data: Intent? = result.data
-                val imageUri: Uri? = data?.data
-                userImage.setImageURI(imageUri)
+                val imageBitmap: Bitmap? = data?.extras?.getString("data") as? Bitmap
+                userImage.setImageBitmap(imageBitmap)
 
-                val utilizadorID = FirebaseAuth.getInstance().currentUser?.uid
-                val imageDrawable = userImage.drawable
-                val imageBitmap = (imageDrawable as BitmapDrawable).bitmap
-
-                guardarFotoPerfil(utilizadorID, imageBitmap)
+                if (imageBitmap != null) {
+                    guardarFotoPerfil(imageBitmap)
+                }
             }
         }
 
@@ -78,59 +73,76 @@ class TelaUtilizador : AppCompatActivity() {
             if (value != null) {
                 userName.setText(value.getString("nome"))
                 userEmail.setText(email)
+                val fotoPerfilUrl = value.getString("fotoPerfilUrl")
+                if (fotoPerfilUrl != null) {
+                    carregarImagem(fotoPerfilUrl)
+                }
             }
         }
 
     }
 
+
     private fun abrirGaleria() {
-        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        pickImage.launch(intent)
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        captureImage.launch(cameraIntent)
     }
 
 
-    private fun guardarFotoPerfil(utilizadorID: String?, bitmap: Bitmap) {
-        if (utilizadorID != null) {
-            val storageRef: StorageReference = FirebaseStorage.getInstance().reference
-                .child("imagens_perfil/$utilizadorID.jpg")
+    private fun guardarFotoPerfil(bitmap: Bitmap) {
 
-            // Converta a imagem bitmap em um array de bytes
-            val baos = ByteArrayOutputStream()
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
-            val data = baos.toByteArray()
+        utilizadorID = FirebaseAuth.getInstance().currentUser?.uid.toString()
 
-            // Faça o upload da imagem para o Firebase Storage
-            storageRef.putBytes(data)
-                .addOnSuccessListener { taskSnapshot ->
-                    storageRef.downloadUrl.addOnSuccessListener { uri ->
-                        atualizarDocumentoUtilizador(utilizadorID, uri.toString())
-                    }
+        val storageRef: StorageReference = FirebaseStorage.getInstance().reference
+            .child("imagens_perfil/$utilizadorID.jpg")
+
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+
+        storageRef.putBytes(data)
+            .addOnSuccessListener {
+                storageRef.downloadUrl.addOnSuccessListener {
+                    atualizarDocumentoUtilizador(utilizadorID, it.toString())
                 }
-                .addOnFailureListener { exception ->
-                    // Ocorreu um erro durante o upload
-                    // Lide com o erro adequadamente
-                    Log.e("TelaUtilizador", "Erro ao fazer upload da imagem: ${exception.message}")
-                }
-        }
+            }
+            .addOnFailureListener {
+                Log.e("TelaUtilizador", "Erro ao fazer upload da imagem: ${it.message}")
+            }
+
     }
 
     private fun atualizarDocumentoUtilizador(utilizadorID: String, imageUrl: String) {
-        // Atualize o documento do utilizador no Firestore com a URL da imagem
-        val documentReference: DocumentReference = db.collection("Utilizadores").document(utilizadorID)
+
+        val documentReference: DocumentReference =
+            db.collection("Utilizadores").document(utilizadorID)
         documentReference.update("fotoPerfilUrl", imageUrl)
             .addOnSuccessListener {
                 Log.d("TelaUtilizador", "URL da imagem salva no Firestore com sucesso.")
             }
-            .addOnFailureListener { exception ->
-                Log.e("TelaUtilizador", "Erro ao salvar URL da imagem no Firestore: ${exception.message}")
+            .addOnFailureListener {
+                Log.e(
+                    "TelaUtilizador",
+                    "Erro ao salvar URL da imagem no Firestore: ${it.message}"
+                )
             }
+
     }
 
+    private fun carregarImagem(url: String) {
+        if (!isDestroyed) {
+            Glide.with(this)
+                .load(url)
+                .apply(RequestOptions.bitmapTransform(CircleCrop()))
+                .into(userImage)
+        }
+    }
 
     private fun iniciarComponentes() {
         userName = findViewById(R.id.userName)
         userEmail = findViewById(R.id.userEmail)
-        userImage = findViewById(R.id.userImage)
         btnDeslogar = findViewById(R.id.btnDeslogar)
+        userImage = findViewById(R.id.userImage)
     }
 }
