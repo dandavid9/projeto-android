83c369014411c42fca69af14c2ba609a02035329
Danilo David <220001205@esg.ipsantarem.pt>
2024-01-14 18:10:16 +0000

    Adicionar mais alguns metodos para o search

--- app/src/main/java/com/example/projeto/TelaPrincipal.kt
+++ app/src/main/java/com/example/projeto/TelaPrincipal.kt
@@ -5,13 +5,31 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.tomtom.sdk.search.Search
+import com.tomtom.sdk.search.SearchCallback
+import com.tomtom.sdk.search.SearchOptions
+import com.tomtom.sdk.search.SearchResponse
+import com.tomtom.sdk.search.common.error.SearchFailure
+import com.tomtom.sdk.search.online.OnlineSearch
+import com.tomtom.sdk.search.ui.SearchResultsView
+import com.tomtom.sdk.search.ui.SearchSuggestionView
 import com.tomtom.sdk.search.ui.SearchView
 import com.tomtom.sdk.search.ui.SearchViewListener
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
 
 class TelaPrincipal : AppCompatActivity() {
     val apiKey = BuildConfig.TOMTOM_API_KEY
+    private lateinit var search: Search
+    private val searchView: SearchView by lazy { findViewById(R.id.search_view) }
+    private val searchResultsView: SearchResultsView by lazy { findViewById(R.id.search_results_view) }
+    private val searchSuggestionView: SearchSuggestionView by lazy { findViewById(R.id.search_suggestion_view) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_principal)
@@ -19,32 +37,61 @@ class TelaPrincipal : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-    //iniciarMapa()
-        pesquisar()
+        search = OnlineSearch.create(this, apiKey)
+        iniciarPesquisa()
 
     }
 
-    fun pesquisar() {
-        val searchView: SearchView by lazy { findViewById(R.id.search_view) }
+    fun iniciarPesquisa() {
 
         searchView.searchViewListener = object : SearchViewListener {
+
+            override fun onCommandInsert(command: String) {
+                //
+            }
+
             override fun onSearchQueryCancel() {
-                /* YOUR CODE GOES HERE */
+                //
             }
 
             override fun onSearchQueryChanged(input: String) {
-                /* YOUR CODE GOES HERE */
-            }
-
-            override fun onCommandInsert(command: String) {
-                /* YOUR CODE GOES HERE */
+                pesquisar()
             }
         }
-
     }
 
-    companion object {
-        private const val LOCATION_PERMISSION_REQUEST_CODE = 101
+    fun pesquisar() {
+        val query = "TomTom"
+        val searchOptions =
+            SearchOptions(query, countryCodes = setOf("NLD", "POL"), limit = 5)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            withContext(Dispatchers.IO) {
+                search.search(searchOptions,
+                    object : SearchCallback {
+                        override fun onFailure(failure: SearchFailure) {
+                            Toast.makeText(
+                                this@TelaPrincipal,
+                                failure.message,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+
+                        override fun onSuccess(result: SearchResponse) {
+                            addSearchResultsToSearchResultsView(result)
+                        }
+                    })
+
+            }
+        }
+    }
+
+
+
+    fun addSearchResultsToSearchResultsView(result: SearchResponse) {
+        //val places: List<PlaceDetails> = result.results.map { it }
+
+        //searchResultsView.update(places, result.summary.query)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
